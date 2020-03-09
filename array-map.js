5b520dd7cbb52dbcbf2b2bc86b2361e0e2f59f2e
jiangmengqi <jiangmengqi@xiaomi.com>
2020-03-09 23:23:29 +0800

    add arraymap

--- array-map.js
+++ array-map.js
@@ -6,18 +6,19 @@ Array.prototype.map = function(fn, context) {
     var O = Object(this),
         len = O.length >>> 0,
         k = 0,
-        res = new Array(len);
+        res = new Array(len),
+        to = 0;
 
     fn = !fn || typeof fn !== 'function' ? function() {} : fn;
 
     while (k < len) {
         if (O.hasOwnProperty(k)) {
-            res[k] = fn.call(context, O[k], k, O);  
+            res[to++] = fn.call(context, O[k], k, O);  
         }
         k++;
     }
 
-    res.length = k;
+    res.length = to;
 
     return res;
 }
