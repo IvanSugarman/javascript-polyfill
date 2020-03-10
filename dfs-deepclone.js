5e363564a39343155f3571a71cd8e454da4ba8f1
蒋梦奇 <jiangmengqi@xiaomi.com>
2020-03-10 11:08:18 +0800

    add deepclone

--- dfs-deepclone.js
+++ dfs-deepclone.js
@@ -1,14 +1,15 @@
 function DFSDeepClone(obj, cache = new WeakMap()) {
-    var dst = Array.isArray(obj) ? [] : {};
+    var dst;
 
     if (cache.has(obj)) {
         return obj;
-    } 
+    }
 
     if (Array.isArray(obj)) {
+        dst = [];
         obj.forEach(val => dst.push(DFSDeepClone(val, cache)));
     } else if (Object.prototype.toString.call(obj) === '[object Object]') {
-        dst = obj.constructor !== Object ? Object.create(obj.constructor.prototype) : {}; 
+        dst = obj.constructor !== Object ? Object.create(obj.constructor.prototype) : {};
 
         cache.set(obj, obj);
         Object.getOwnPropertyNames(obj).concat(Object.getOwnPropertySymbols(obj)).forEach(val => {
