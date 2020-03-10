c21571ac0be64d8574e6653524718bc9930973dd
蒋梦奇 <jiangmengqi@xiaomi.com>
2020-03-10 11:21:04 +0800

    add bind.js

--- bind.js
+++ bind.js
@@ -11,10 +11,11 @@ Function.prototype.bind = function(context) {
     args = Array.prototype.slice.call(arguments, 1);
 
     fBound = function() {
-        fn.apply(fn instanceof context ? fNop : context, args.concat(Array.prototype.slice.call(arguments)))
+        // new.target
+        return fn.apply(fn instanceof fNop ? fn : context, args.concat(Array.prototype.slice.call(arguments)));
     }
 
-    fNop.prototype = context.prototype;
+    fNop.prototype = fn.prototype;
     fBound.prototype = new fNop();
 
     return fBound;
