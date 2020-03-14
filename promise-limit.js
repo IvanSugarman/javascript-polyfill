866bc109b6d81b4283b577d2949b17a84b926c07
jiangmengqi <jiangmengqi@xiaomi.com>
2020-03-14 19:08:42 +0800

    promise limit

--- promise-limit.js
+++ promise-limit.js
@@ -41,3 +41,51 @@ function promiseLimit(promises, limit) {
 
     return new Promise(init);
 }
+
+// Blue Bird Promise.map
+class Limit {
+    constructor(limit) {
+        this.limit = limit;
+        this.queue = [];
+        this.count = 0;
+    }
+
+    enqueue(fn) {
+        return new Promise(resolve, reject) => {
+            this.queue.push({ fn, resolve, reject });
+        }); 
+    }
+
+    dequeue() {
+        if (this.count < this.limit && this.queue.length) {
+            const { fn, resolve, reject } = this.queue.shift();
+            this.run(fn).then(resolve).catch(reject);
+        }
+    }
+
+    async run(fn) {
+        this.count++;
+        const val = await fn();
+        this.count--;
+        this.dequeue();
+
+        return val;
+    }
+
+    build(fn) {
+        // 超过阈值放入队列
+        if (this.count < this.limit) {
+            return this.run(fn);
+        } else {
+            return this.enqueue(fn);
+        } 
+    } 
+}
+
+Promise.map = function(list, fn, { concurrency }) {
+    const limit = new Limit(concurrency);
+
+    return Promise.all(list.map => ((...args) => {
+        return limit.build(() => fn(...args));
+    }));
+}
